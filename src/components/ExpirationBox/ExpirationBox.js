9859785772a02601d2c028a95036526439823edb
Darrin Daugherty <darrin@darrin.pro>
2023-11-13 13:58:02 -0500

    began modals in employee search

---
+++ src/components/ExpirationBox/ExpirationBox.js
@@ -0,0 +1,16 @@
+import './ExpirationBox.css'
+
+export default function ExpirationBox() {
+    return (
+        <div className="expiration-box">
+        <h1>Upcoming CPR Expirations</h1>
+        <p>Please schedule these employees for CPR certification ASAP</p>
+        <ul>
+            <li>Jane Sanders</li>
+            <li>John Harris</li>
+            <li>Sam Smith</li>
+            <li>Emily Jackson</li>
+        </ul>
+        </div>
+    );
+}
